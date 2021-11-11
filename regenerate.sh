7a4b48f88076d9a79b8c44f649a41497d83d5540
Turing Zhu <qishiwenjun@163.com>
2021-11-12 01:38:20 +0800

    [feat] set package name

--- regenerate.sh
+++ regenerate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-openapi-generator generate -i "api/openapi.yaml" --skip-validate-spec -c "api/openapitools.json" -g go
-# -c "api/openapitools.json"
+openapi-generator generate \
+  -i "api/openapi.yaml" \
+  --skip-validate-spec \
+  -c "api/openapitools.json" \
+  -g go \
+  --git-user-id Turing-Chu \
+  --git-repo-id binanceapi-go
